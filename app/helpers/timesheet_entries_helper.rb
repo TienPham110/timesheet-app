bf4cdee84b9095f0bb29527e37d93399a9e1aa95
tienpham <phamhongtien19@gmail.com>
2022-09-30 18:22:26 +0700

    using helper for view

--- app/helpers/timesheet_entries_helper.rb
+++ app/helpers/timesheet_entries_helper.rb
@@ -1,2 +1,9 @@
 module TimesheetEntriesHelper
+  def formatted_date(timesheet)
+    timesheet.date.strftime("%d/%m/%Y")
+  end
+  
+  def time_range(timesheet)
+    "#{Time.at(timesheet.start_time).utc.strftime("%H:%M")} - #{Time.at(timesheet.finish_time).utc.strftime("%H:%M")}"
+  end
 end
--- app/models/timesheet_entry.rb
+++ app/models/timesheet_entry.rb
@@ -1,5 +1,5 @@
 class TimesheetEntry < ApplicationRecord
   validates :date, :start_time, :finish_time, presence: { message: "is required" }
-  validates :date, comparison: { less_than: Date.today, message: "can't be in the future" }
+  validates :date, comparison: { less_than_or_equal_to: Date.today, message: "can't be in the future" }
   validates :finish_time, comparison: { greater_than: :start_time, message: "can't be before start time" }
 end
