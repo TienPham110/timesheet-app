fffc0905a85e5b0a2fe159b878f1c515886d4359
tienpham <phamhongtien19@gmail.com>
2022-10-01 18:35:16 +0700

    validate overlapping timesheet entries

--- app/models/timesheet_entry.rb
+++ app/models/timesheet_entry.rb
@@ -2,6 +2,10 @@ class TimesheetEntry < ApplicationRecord
   validates :date, :start_time, :finish_time, presence: { message: "is required" }
   validates :date, comparison: { less_than_or_equal_to: Date.today, message: "can't be in the future" }
   validates :finish_time, comparison: { greater_than_or_equal_to: :start_time, message: "can't be before start time" }
+  validate :cannot_overllap_timesheet
+
+  scope :in_range, -> range { where(start_time: range).or(where(finish_time: range)) }
+  scope :by_date, -> date { where(date: date) }
 
   RATE = { # In dollar $
     INSIDE_EVEN_DAY: 22, # Even days are monday, wednesday, friday
@@ -15,4 +19,15 @@ class TimesheetEntry < ApplicationRecord
     EVEN_DAY: 25200..68400, # 7am - 7pm
     ODD_DAY: 18000..61200 # 5am - 5pm
   }
+
+  private
+    def cannot_overllap_timesheet
+      return unless date && start_time && finish_time
+
+      overlaps = TimesheetEntry
+        .by_date(date)
+        .in_range(start_time..finish_time)
+        
+      errors.add(:base, "Can't have overlapping timesheet entries") unless overlaps.empty?
+    end
 end
